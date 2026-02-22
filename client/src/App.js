7a694274ea75cf33ef7f87357df00c5dbcf524f9
Nehal Mehta <nehal29096@gmail.com>
2026-02-22 22:12:55 +0530

    navbar hidden for register, login and landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Landing from "./pages/Landing.js";
 import Register from "./pages/Register.js";
 import Login from "./pages/Login.js";
@@ -7,10 +7,14 @@ import Home from "./pages/Home.js";
 import { ToastContainer } from "react-toastify";
 import Navbar from "./components/NavBar.js";
 
-function App(){
+function AppContent(){
+  const location = useLocation();
+
+  const hideNavbarRoutes = ["/", "/login", "/register"];
   return (
-    <BrowserRouter>
-      <Navbar/>
+    <>
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Landing/>}/>
         <Route path="/register" element={<Register/>}/>
@@ -18,8 +22,16 @@ function App(){
         <Route path="/home" element={<Home />} />
       </Routes>
       
-      <ToastContainer position="top-right" autoClose={3000} />
+      <ToastContainer position="bottom-right" autoClose={3000} />
 
+    </>
+  )
+}
+
+function App(){
+  return (
+    <BrowserRouter>
+      <AppContent/>
     </BrowserRouter>
   )
 }
