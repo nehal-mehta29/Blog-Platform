ad7b33dd2797685a25b4f702e7c195f3fa012c2e
Nehal Mehta <nehal29096@gmail.com>
2026-02-26 11:56:29 +0530

    Single post view

--- Server/controllers/postController.js
+++ Server/controllers/postController.js
@@ -1,12 +1,70 @@
-export const getAllPosts = async (req, res) => {
+import Post from "../Schema/Post.js";
+import mongoose from "mongoose";
+
+//CREATE POST
+export const createPost = async (req, res) => {
     try {
+        const { title, content } = req.body;
+        
+        if (!title || !content) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        
+        const newPost = new Post({
+            title,
+            content,
+            author: req.user.id
+        })
+      
+        const savedPost = await newPost.save();
+      
+        res.status(201).json({
+            message: "Post created successfully",
+            post: savedPost
+        })
+    }
+
+    catch(error){
+        res.status(500).json({message: "Server error"});
+    }
+}
+
+//GET ALL POSTS
+export const getAllPosts = async(req, res) => {
+    try{
         const posts = await Post.find()
-          .populate("author", "username")
-          .sort({ createdAt: -1 });
+        .populate("author","username")      //only return username
+        .sort({createdAt: -1});             //newest first
 
         res.status(200).json(posts);
-    } 
-    catch (error) {
-        res.status(500).json({ message: "Server error" });
     }
-};
+
+    catch(error){
+        console.error("Error fetching posts: ",error);
+        res.status(500).json({message: "Server error"});
+    }
+}
+
+//GET SINGLE POST
+export const getSinglePost = async (req, res) => {
+    try{
+        const{id} = req.params;
+
+        //Validate MongoDB objectId
+        if (!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({message : "Invalid post ID"});
+        }
+
+        const post = await Post.findById(id).populate("author", "username");
+
+        if(!post){
+            return res.status(404).json({message: "Post not found"});
+        }
+
+        res.status(200).json(post);
+    }
+
+    catch(error){
+        res.status(500).json({message: "Server error"});
+    }
+}
--- Server/routes/postRoutes.js
+++ Server/routes/postRoutes.js
@@ -1,51 +1,16 @@
 import express from "express";
-import Post from "../Schema/Post.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import authMiddleare from "../middleware/authMiddleware.js";
+import { createPost, getAllPosts, getSinglePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
-//GET ALL POSTS
-router.get("/", async(req, res) => {
-    try{
-        const posts = await Post.find()
-        .populate("author","username")      //only return username
-        .sort({createdAt: -1});             //newest first
+//Create Post
+router.post("/",authMiddleare, createPost);
 
-        res.status(200).json(posts);
-    }
+//Get all Post
+router.get("/", getAllPosts);
 
-    catch(error){
-        console.error("Error fetching posts: ",error);
-        res.status(500).json({message: "Server error"});
-    }
-})
-
-//CREATE POST
-router.post("/", authMiddleware, async (req, res) => {
-    try {
-        const { title, content } = req.body;
-        
-        if (!title || !content) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-        
-        const newPost = new Post({
-            title,
-            content,
-            author: req.user.id
-        })
-      
-        const savedPost = await newPost.save();
-      
-        res.status(201).json({
-            message: "Post created successfully",
-            post: savedPost
-        })
-    }
-
-    catch(error){
-        res.status(500).json({message: "Server error"});
-    }
-})
+//Get single post
+router.get("/", getSinglePost);
 
 export default router;
